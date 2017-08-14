7bd3a4ce23912a4cd9d2556ddf134c51aae1e52f
jeongjoonsup <penny4860@gmail.com>
2017-08-14 14:09:14 +0900

    model, runner 분리

--- main.py
+++ main.py
@@ -18,12 +18,9 @@ from keras import backend as K
 class Visualizer(object):
     """CNN model의 layer를 activation하는 image를 generation하는 class"""
     
-    def __init__(self, cnn_model, layer_name, filter_index):
+    def __init__(self, cnn_model, layer_name):
         self.cnn_model = cnn_model
-        
         self.layer_op = self._create_activation_op(layer_name)
-        self.loss_op = self._create_loss_op(filter_index)
-        self.grad_op = self._create_grad_op()
 
     def _create_activation_op(self, layer_name):
         layer_dict = dict([(layer.name, layer) for layer in self.cnn_model.layers[1:]])
@@ -33,18 +30,43 @@ class Visualizer(object):
         loss = K.mean(self.layer_op[:, :, :, filter_index])
         return loss
     
-    def _create_grad_op(self):
+    def create_grad_op(self, filter_index):
         def normalize(x):
             # utility function to normalize a tensor by its L2 norm
             return x / (K.sqrt(K.mean(K.square(x))) + 1e-5)
 
-        grads = K.gradients(self.loss_op, self.cnn_model.input)[0]
-        grads = normalize(grads)
-        return grads
+        loss_op = self._create_loss_op(filter_index)
+        grads_op = K.gradients(loss_op, self.cnn_model.input)[0]
+        grads_op = normalize(grads_op)
+        return grads_op, loss_op
+
+
+class VisualizerRunner:
     
-    def recon(self, w, h, iterations=20):
+    def __init__(self, visualizer):
+        self.vis = visualizer
+    
+    def run(self, w, h, n_filters=4, iterations=20):
+        
+        kept_filters = []
+        for i in range(0, n_filters):
+    
+            start_time = time.time()
+            img, loss = self._recon(w, h, i, iterations)
+            end_time = time.time()
+            
+            if loss > 0:
+                kept_filters.append((img, loss))
+    
+            print('Filter %d processed in %ds' % (i, end_time - start_time))
+        print(len(kept_filters))
+        
+    def _recon(self, w, h, filter_index, iterations):
+        
         # this function returns the loss and grads given the input picture
-        iterate_op = K.function([self.cnn_model.input], [self.loss_op, self.grad_op])
+        grads, loss = self.vis.create_grad_op(filter_index)
+        
+        iterate_op = K.function([self.vis.cnn_model.input], [loss, grads])
         image = self._initialize_random_image(w, h)
 
         # we run gradient ascent for 20 steps
@@ -116,27 +138,11 @@ if __name__ == '__main__':
     n_filters = 4
     step = 1.                       # step size for gradient ascent
     n = 2                           # we will stich the best 64 filters on a 8 x 8 grid.
-
-    # 1. build the VGG16 network with ImageNet weights
     model = vgg16.VGG16(weights='imagenet', include_top=False)
-    kept_filters = []
-    
-    for i in range(0, n_filters):
 
-        start_time = time.time()
-        
-        vis = Visualizer(model, layer_name, filter_index=i)
-        img, loss = vis.recon((img_height, img_width), 2)
-        
-        end_time = time.time()
-        
-        if loss > 0:
-            kept_filters.append((img, loss))
+    vis = Visualizer(model, layer_name)
+    runner = VisualizerRunner(vis)
+    kept_filters = runner.run(img_width, img_height, n_filters=4, iterations=2)
 
-        print('Filter %d processed in %ds' % (i, end_time - start_time))
-    
-    print(len(kept_filters))
-    
-    draw_image(kept_filters, img_width, img_height, n)
 
 
