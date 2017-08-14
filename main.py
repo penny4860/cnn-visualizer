18da20bdc25a37f58152b1c3491b75589f0f5024
jeongjoonsup <penny4860@gmail.com>
2017-08-14 13:20:50 +0900

    class 로 정리

--- main.py
+++ main.py
@@ -15,6 +15,52 @@ from keras import backend as K
 # util function to convert a tensor into a valid image
 
 
+class Visualizer(object):
+    
+    def __init__(self, cnn_model, layer_name, filter_index):
+        self.cnn_model = cnn_model
+        self.loss_op = self._create_loss_op(layer_name, filter_index)
+        self.grad_op = self._create_grad_op()
+
+    def _create_loss_op(self, layer_name, filter_index):
+        layer_dict = dict([(layer.name, layer) for layer in self.cnn_model.layers[1:]])
+        layer_activation = layer_dict[layer_name].output
+        loss = K.mean(layer_activation[:, :, :, filter_index])
+        return loss
+    
+    def _create_grad_op(self):
+        def normalize(x):
+            # utility function to normalize a tensor by its L2 norm
+            return x / (K.sqrt(K.mean(K.square(x))) + 1e-5)
+
+        grads = K.gradients(self.loss_op, self.cnn_model.input)[0]
+        grads = normalize(grads)
+        return grads
+    
+    def recon(self, w, h, iterations=20):
+        # this function returns the loss and grads given the input picture
+        iterate_op = K.function([self.cnn_model.input], [self.loss_op, self.grad_op])
+        image = self._initialize_random_image(w, h)
+
+        # we run gradient ascent for 20 steps
+        for _ in range(iterations):
+            loss_value, grads_value = iterate_op([image])
+            
+            image += grads_value * step
+    
+            print('Current loss value:', loss_value)
+            if loss_value <= 0.:
+                # some filters get stuck to 0, we can skip them
+                break
+    
+        return image, loss_value
+
+    def _initialize_random_image(self, w, h):
+        # we start from a gray image with some random noise
+        image = np.random.random((1, img_width, img_height, 3))
+        image = (image - 0.5) * 20 + 128
+        return image
+
 def deprocess_image(x):
     # normalize tensor: center on 0., ensure std is 0.1
     x -= x.mean()
@@ -31,55 +77,6 @@ def deprocess_image(x):
         x = x.transpose((1, 2, 0))
     x = np.clip(x, 0, 255).astype('uint8')
     return x
-
-def normalize(x):
-    # utility function to normalize a tensor by its L2 norm
-    return x / (K.sqrt(K.mean(K.square(x))) + 1e-5)
-
-def create_loss_tensor(layer_name, filter_index):
-    # we build a loss function that maximizes the activation
-    # of the nth filter of the layer considered
-    layer_output = layer_dict[layer_name].output
-    if K.image_data_format() == 'channels_first':
-        loss = K.mean(layer_output[:, filter_index, :, :])
-    else:
-        loss = K.mean(layer_output[:, :, :, filter_index])
-    return loss
-
-def create_grad_tensor(loss, variables):
-    # we compute the gradient of the input picture wrt this loss
-    # normalization trick: we normalize the gradient
-    grads = K.gradients(loss, variables)[0]
-    grads = normalize(grads)
-    return grads
-
-def random_gray_image():
-    # we start from a gray image with some random noise
-    if K.image_data_format() == 'channels_first':
-        image = np.random.random((1, 3, img_width, img_height))
-    else:
-        image = np.random.random((1, img_width, img_height, 3))
-    image = (image - 0.5) * 20 + 128
-    return image
-
-def gradient_ascent(loss_op, grads_op, input_img_pl, input_img_data):
-
-    # this function returns the loss and grads given the input picture
-    iterate = K.function([input_img_pl], [loss_op, grads_op])
-
-    # we run gradient ascent for 20 steps
-    for _ in range(4):
-        loss_value, grads_value = iterate([input_img_data])
-        print(input_img_data.shape, grads_value.shape)
-        
-        input_img_data += grads_value * step
-
-        print('Current loss value:', loss_value)
-        if loss_value <= 0.:
-            # some filters get stuck to 0, we can skip them
-            break
-
-    return input_img_data, loss_value
 
 def draw_image(filters, img_width, img_height, n, margin = 5):
     
@@ -117,30 +114,24 @@ if __name__ == '__main__':
 
     # 1. build the VGG16 network with ImageNet weights
     model = vgg16.VGG16(weights='imagenet', include_top=False)
-    print('Model loaded.')
-    model.summary()
-    # get the symbolic outputs of each "key" layer (we gave them unique names).
-    layer_dict = dict([(layer.name, layer) for layer in model.layers[1:]])
-
     kept_filters = []
-    for filter_index in range(0, n_filters):
-        # we only scan through the first 200 filters,
-        # but there are actually 512 of them
-        print('Processing filter %d' % filter_index)
-        start_time = time.time()
-
-        # create loss operation
-        loss = create_loss_tensor(layer_name, filter_index)
-        grads = create_grad_tensor(loss, model.input)
-        # input_img_data (1, 128, 128, 3)
-        input_img_data, loss_value = gradient_ascent(loss, grads, model.input, random_gray_image())
     
-        # decode the resulting input image
-        if loss_value > 0:
-            img = deprocess_image(input_img_data[0])
-            kept_filters.append((img, loss_value))
+    for i in range(0, n_filters):
+
+        start_time = time.time()
+        
+        vis = Visualizer(model, layer_name, filter_index=i)
+        img, loss = vis.recon((img_height, img_width), 2)
+        
         end_time = time.time()
-        print('Filter %d processed in %ds' % (filter_index, end_time - start_time))
+        
+        if loss > 0:
+            img = deprocess_image(img[0])
+            kept_filters.append((img, loss))
+
+        print('Filter %d processed in %ds' % (i, end_time - start_time))
+    
+    print(len(kept_filters))
     
     draw_image(kept_filters, img_width, img_height, n)
 
