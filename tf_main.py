4ad4229db4d8c8b342105c2852469b2fe00586a2
jeongjoonsup <penny4860@gmail.com>
2017-08-15 13:24:21 +0900

    code cleaning

--- tf_main.py
+++ tf_main.py
@@ -16,23 +16,32 @@ np.set_printoptions(precision=5, linewidth=2000, suppress=True)
 
 class ImgGenerator:
     
-    def __init__(self, activation):
-        self.loss_op = tf.reduce_mean(activation)
+    def __init__(self, input_tensor, activation):
+        self.loss_op = self._create_loss_op(activation)
+        self.grads_op = self._create_gradient_op()
+        
+    def _create_loss_op(self, activation):
+        return tf.reduce_mean(activation)
+
+    def _create_gradient_op(self):
         grads_op = tf.gradients(self.loss_op, X)[0]
-        self.grads_op = grads_op / tf.sqrt(tf.reduce_mean(tf.square(grads_op))) + tf.constant(1e-5)
-    
+        grads_op = grads_op / tf.sqrt(tf.reduce_mean(tf.square(grads_op))) + tf.constant(1e-5)
+        return grads_op
+
 
 filter_index = 0
 if __name__ == '__main__':
     
-    # 1. Build graph
+    # 1. Input Tensor
     X = tf.placeholder(tf.float32, [None, 128, 128, 3])
+
+    # 2. VGG network instance
     vggnet = Vgg16(X)
 
-    # 2. Image Generator
-    gen = ImgGenerator(vggnet.conv5_1[:, :, :, filter_index])
+    # 3. Image Generator instance
+    gen = ImgGenerator(X, vggnet.conv5_1[:, :, :, filter_index])
     
-    # 3. session
+    # 4. session
     init_op = tf.global_variables_initializer()
     with tf.Session() as sess:
         sess.run(init_op)
