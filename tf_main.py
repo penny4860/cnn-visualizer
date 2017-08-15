6da9b329d63928bbc207f3d03714227348f8df6f
jeongjoonsup <penny4860@gmail.com>
2017-08-15 14:08:39 +0900

    nit

--- tf_main.py
+++ tf_main.py
@@ -3,14 +3,10 @@
 # Visualization of the filters of VGG16, via gradient ascent in input space.
 
 from __future__ import print_function
-
 import tensorflow as tf
-import numpy as np
 
 from src.utils import ImgGenerateModel, recon, plot_images
 from src.vgg import Vgg16
-
-np.set_printoptions(precision=5, linewidth=2000, suppress=True)
 
 if __name__ == '__main__':
     # 1. Input Tensor
