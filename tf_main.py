70f02edb7678b29411b1eb5ddd86d4ec24a071a9
jeongjoonsup <penny4860@gmail.com>
2017-08-15 13:20:30 +0900

    ImgGenerator class 구현

--- tf_main.py
+++ tf_main.py
@@ -14,6 +14,14 @@ from src.vgg import Vgg16
 np.set_printoptions(precision=5, linewidth=2000, suppress=True)
 
 
+class ImgGenerator:
+    
+    def __init__(self, activation):
+        self.loss_op = tf.reduce_mean(activation)
+        grads_op = tf.gradients(self.loss_op, X)[0]
+        self.grads_op = grads_op / tf.sqrt(tf.reduce_mean(tf.square(grads_op))) + tf.constant(1e-5)
+    
+
 filter_index = 0
 if __name__ == '__main__':
     
@@ -21,12 +29,9 @@ if __name__ == '__main__':
     X = tf.placeholder(tf.float32, [None, 128, 128, 3])
     vggnet = Vgg16(X)
 
-    # 2. activation_op / loss_op / grads_op
-    activation_op = vggnet.conv5_1
-    loss_op = tf.reduce_mean(activation_op[:,:,:,filter_index])
-    grads_op = tf.gradients(loss_op, X)[0]
-    grads_op = grads_op / tf.sqrt(tf.reduce_mean(tf.square(grads_op))) + tf.constant(1e-5)
-
+    # 2. Image Generator
+    gen = ImgGenerator(vggnet.conv5_1[:, :, :, filter_index])
+    
     # 3. session
     init_op = tf.global_variables_initializer()
     with tf.Session() as sess:
@@ -34,7 +39,7 @@ if __name__ == '__main__':
         vggnet.load_ckpt(sess)
         image = initialize_random_images(random_seed=111)
         for i in range(20):
-            loss_value, grads_value = sess.run([loss_op, grads_op], feed_dict={X:image})
+            loss_value, grads_value = sess.run([gen.loss_op, gen.grads_op], feed_dict={X:image})
             image += grads_value
             print("Iter : {}, activation_score : {}".format(i, loss_value))
 
