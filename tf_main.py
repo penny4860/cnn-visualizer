0984036ced0a260a968efaf738a2cd812b4b2813
jeongjoonsup <penny4860@gmail.com>
2017-08-15 12:57:00 +0900

    build_vgg16() deleted

--- tf_main.py
+++ tf_main.py
@@ -52,29 +52,6 @@ class Vgg16(object):
         self.conv5_2 = slim.conv2d(self.conv4_1, 512, [3, 3], scope='vgg_16/conv5/conv5_2')
         self.conv5_3 = slim.conv2d(self.conv4_2, 512, [3, 3], scope='vgg_16/conv5/conv5_3')
 
-def build_vgg16(input_tensor):
-    net = slim.conv2d(input_tensor, 64, [3, 3], scope='vgg_16/conv1/conv1_1')
-    net = slim.conv2d(net, 64, [3, 3], scope='vgg_16/conv1/conv1_2')
-    net = slim.max_pool2d(net, [2, 2], scope='pool1')
-    
-    net = slim.conv2d(net, 128, [3, 3], scope='vgg_16/conv2/conv2_1')
-    net = slim.conv2d(net, 128, [3, 3], scope='vgg_16/conv2/conv2_2')
-    net = slim.max_pool2d(net, [2, 2], scope='pool2')
-    
-    net = slim.conv2d(net, 256, [3, 3], scope='vgg_16/conv3/conv3_1')
-    net = slim.conv2d(net, 256, [3, 3], scope='vgg_16/conv3/conv3_2')
-    net = slim.conv2d(net, 256, [3, 3], scope='vgg_16/conv3/conv3_3')
-    net = slim.max_pool2d(net, [2, 2], scope='pool3')
-     
-    net = slim.conv2d(net, 512, [3, 3], scope='vgg_16/conv4/conv4_1')
-    net = slim.conv2d(net, 512, [3, 3], scope='vgg_16/conv4/conv4_2')
-    net = slim.conv2d(net, 512, [3, 3], scope='vgg_16/conv4/conv4_3')
-    net = slim.max_pool2d(net, [2, 2], scope='pool4')
-     
-    net = slim.conv2d(net, 512, [3, 3], scope='vgg_16/conv5/conv5_1')
-#     net = slim.conv2d(net, 512, [3, 3], scope='vgg_16/conv5/conv5_2')
-#     net = slim.conv2d(net, 512, [3, 3], scope='vgg_16/conv5/conv5_3')
-    return net
 
 filter_index = 0
 if __name__ == '__main__':
